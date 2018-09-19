b914b278ea8d8ed491a24f3bc3c06fda3140471c
Max Shaw <maxpshaw@gmail.com>
2018-09-19 01:02:54 -0700

    section8#105

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -22,4 +22,52 @@ class Fib extends Component {
     const seenIndexes = await axios.get('/api/values/all');
     this.setState({ seenIndexes: values.data });
   }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index: this.state.index
+    });
+
+    this.setState({ index: '' });
+  };
+
+  renderSeenIndexes() {
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+  }
+
+  rednerValues() {
+    const entries = [];
+
+    for (let key in this.state.values) {
+      entris.push(
+        <div key={key}>
+          For index {key} I calculated {this.state.values[key]}
+        </div>
+      );
+    }
+
+    return entries;
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            values={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
+
+        <h3>Indexes I have seen:</h3>
+        {this.renderSeenIndexes()}
+        <h3>Calculated Values</h3>
+        {this.rednerValues()}
+      </div>
+    );
+  }
 }
